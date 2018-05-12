678cbaa72d58d19af78f2f5041ca6e9fa9efa96f
Wallace Coelho <wac.almeida@gmail.com>
2018-05-12 11:21:07 -0300

    chore: minify css on production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,75 @@
 const path = require('path');
 
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (env, argv) => {
-  console.log('env', env);
-  console.log('arg', argv);
-
-  return {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve('dist'),
-      filename: 'bundle.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-          },
+module.exports = (_, argv) => ({
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
         },
-        {
-          test: /\.(png|jpg|gif|svg|eot|woff|woff2|ttf)$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 8192,
-              },
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|woff|woff2|ttf)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
             },
-          ],
-        },
-        {
-          test: /\.css$/,
-          use: [
-            process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-            'css-loader',
-          ],
-        },
-        {
-          test: /\.less$/,
-          use: [
-            process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-            'css-loader',
-            'less-loader',
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve('./src/index.html'),
-      }),
-      new CopyWebpackPlugin([
-        {
-          from: './src/assets/images',
-          to: 'images',
-        },
-      ]),
-      new MiniCssExtractPlugin({
-        filename: '[name].css',
-        chunkFilename: '[id].css',
-      }),
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          argv.mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          argv.mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
     ],
-    devServer: {
-      contentBase: path.join(__dirname, 'dist'),
-      compress: true,
-      port: 8000,
-    },
-  };
-};
+  },
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({})],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./src/index.html'),
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: './src/assets/images',
+        to: 'images',
+      },
+    ]),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8000,
+  },
+});
