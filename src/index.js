340402df1324568d467f1627780d2adfcaef0738
Wallace Coelho <wac.almeida@gmail.com>
2018-05-12 10:55:53 -0300

    chore: fix eslint code style

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './assets/less/styles.less';
 
-jQuery(document).ready(function($) {
-  /*======= Skillset *=======*/
+jQuery(document).ready(($) => {
+  /*= ====== Skillset *======= */
   $('.level-bar-inner').css('width', '0');
-  $(window).on('load', function() {
-      $('.level-bar-inner').each(function() {
-          var itemWidth = $(this).data('level');
-          $(this).animate({
-              width: itemWidth
-          }, 800);
-      });
+  $(window).on('load', () => {
+    $('.level-bar-inner').each(() => {
+      const itemWidth = $(this).data('level');
+      $(this).animate({
+        width: itemWidth,
+      }, 800);
+    });
   });
 });
